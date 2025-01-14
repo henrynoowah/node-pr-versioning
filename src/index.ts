3eb135e261d0cf558911fc2d1f894580b562d935
조하운 <79410456+henrynoowah@users.noreply.github.com>
2025-01-14 12:35:42 +0900

    chore: parse version to number before updating

--- src/index.ts
+++ src/index.ts
@@ -51,14 +51,17 @@ async function run() {
     if (isMajor) {
       console.log("Major label found");
 
-      const newVersion = version.split(".")[0] + 1 + ".0.0";
+      const newVersion = Number(version.split(".")[0]) + 1 + ".0.0";
 
       console.log(newVersion);
     }
     if (isMinor) {
       console.log("Minor label found");
       const newVersion =
-        version.split(".")[0] + "." + version.split(".")[1] + 1 + ".0";
+        version.split(".")[0] +
+        "." +
+        (Number(version.split(".")[1]) + 1) +
+        ".0";
       console.log(newVersion);
     }
     if (isPatch) {
@@ -68,8 +71,7 @@ async function run() {
         "." +
         version.split(".")[1] +
         "." +
-        version.split(".")[2] +
-        1;
+        (Number(version.split(".")[2]) + 1);
       console.log(newVersion);
     }
 

d5d86d02794fedae2cfe1d5c1871721201d25838
조하운 <79410456+henrynoowah@users.noreply.github.com>
2025-01-14 11:43:14 +0900

    chore: github actions implementation

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import { getInput, setFailed } from "@actions/core";
-// import { context, getOctokit } from "@actions/github";
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
 const fs_1 = require("fs");
 async function run() {
-    // const token = getInput("gh-token");
-    // const label = getInput("label");
-    // const minorLabel = getInput("minor-label");
-    // const majorLabel = getInput("major-label");
-    // const patchLabel = getInput("patch-label");
+    const token = (0, core_1.getInput)("gh-token");
+    const label = (0, core_1.getInput)("label");
+    const minorLabel = (0, core_1.getInput)("minor-label");
+    const majorLabel = (0, core_1.getInput)("major-label");
+    const patchLabel = (0, core_1.getInput)("patch-label");
     const packageJsonPath = `${process.cwd()}/package.json`;
     const packageJson = JSON.parse((0, fs_1.readFileSync)(packageJsonPath, "utf-8"));
     const version = packageJson.version;
     console.log(version);
-    // const octokit = getOctokit(token);
-    // const pullRequest = context.payload.pull_request;
-    // try {
-    //   if (!pullRequest) {
-    //     setFailed("This action should only be run on a pull request");
-    //     return;
-    //   }
-    //   await octokit.rest.issues.addLabels({
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     issue_number: pullRequest.number,
-    //     labels: [label],
-    //   });
-    // } catch (error) {
-    //   setFailed((error as Error).message);
-    // }
+    const octokit = (0, github_1.getOctokit)(token);
+    const pullRequest = github_1.context.payload.pull_request;
+    try {
+        if (!pullRequest) {
+            (0, core_1.setFailed)("This action should only be run on a pull request");
+            return;
+        }
+        await octokit.rest.issues.addLabels({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+    }
+    catch (error) {
+        (0, core_1.setFailed)(error.message);
+    }
 }
 run();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-// import { getInput, setFailed } from "@actions/core";
-// import { context, getOctokit } from "@actions/github";
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 import { readFileSync } from "fs";
 
 async function run() {
-  // const token = getInput("gh-token");
-  // const label = getInput("label");
+  const token = getInput("gh-token");
+  const label = getInput("label");
 
-  // const minorLabel = getInput("minor-label");
-  // const majorLabel = getInput("major-label");
-  // const patchLabel = getInput("patch-label");
+  const minorLabel = getInput("minor-label");
+  const majorLabel = getInput("major-label");
+  const patchLabel = getInput("patch-label");
 
   const packageJsonPath = `${process.cwd()}/package.json`;
   const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
@@ -16,24 +16,24 @@ async function run() {
 
   console.log(version);
 
-  // const octokit = getOctokit(token);
+  const octokit = getOctokit(token);
 
-  // const pullRequest = context.payload.pull_request;
+  const pullRequest = context.payload.pull_request;
 
-  // try {
-  //   if (!pullRequest) {
-  //     setFailed("This action should only be run on a pull request");
-  //     return;
-  //   }
-  //   await octokit.rest.issues.addLabels({
-  //     owner: context.repo.owner,
-  //     repo: context.repo.repo,
-  //     issue_number: pullRequest.number,
-  //     labels: [label],
-  //   });
-  // } catch (error) {
-  //   setFailed((error as Error).message);
-  // }
+  try {
+    if (!pullRequest) {
+      setFailed("This action should only be run on a pull request");
+      return;
+    }
+    await octokit.rest.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      labels: [label],
+    });
+  } catch (error) {
+    setFailed((error as Error).message);
+  }
 }
 
 run();
